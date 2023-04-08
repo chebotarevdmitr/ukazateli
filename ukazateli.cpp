fbf14f61722863d21bbd2fc046ae089669f14db7
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-04-08 22:06:51 +0300

    Добавьте файлы проекта.

---
+++ ukazateli.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void printNumber(int* numberPtr) {
+    cout << *numberPtr << endl;
+}
+
+int main()
+{
+    int number = 5;
+
+
+
+
+        system("pause>0");
+        return 0;
+}
