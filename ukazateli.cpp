d8bbdcac3c5eb8b46d94054aa7819048aaed8a4b
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-04-08 22:45:30 +0300

    test2

--- ukazateli.cpp
+++ ukazateli.cpp
@@ -5,12 +5,17 @@ using namespace std;
 void printNumber(int* numberPtr) {
     cout << *numberPtr << endl;
 }
+void printLetter(char* charPtr) {
+    cout << *charPtr << endl;
+}
+
 
 int main()
 {
     int number = 5;
-
-
+    char letter = 'a';
+    printNumber(&number);
+    printLetter(&letter);
 
 
         system("pause>0");
